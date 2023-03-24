5549117ada07a679503c770b90eb2ea3e34483f7
limmylem <72368168+limmylem@users.noreply.github.com>
2023-03-25 08:34:31 +1030

    small bug fixes

--- SkillTree.java
+++ SkillTree.java
@@ -1,5 +1,3 @@
-import com.sun.source.tree.Tree;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -16,132 +14,113 @@ public class SkillTree {
         JPanel MainPanel = new JPanel();
         MainPanel.setLayout(new FlowLayout());
         JButton ExitButton = new JButton("Exit");
-        ExitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        ExitButton.addActionListener(e -> System.exit(0));
 
         MainPanel.add(ExitButton);
 
         JButton skillTreeButton = new JButton("Skill Tree");
-        skillTreeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFrame SkillTreeFrame = new JFrame("Skill Tree");
-                SkillTreeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                SkillTreeFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                Main.dispose();
-                // add the code for the popup menu here
-                JPopupMenu popupMenu = new JPopupMenu();
-                JMenuItem CreateSkill = new JMenuItem("Create Skill");
-                CreateSkill.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        String SkillName = JOptionPane.showInputDialog(null, "Enter skill name:");
-                        String SkillGoal = JOptionPane.showInputDialog(null, "Enter the skills goal:");
-                        String SkillXP = JOptionPane.showInputDialog(null, "Enter the skills XP:");
-                        String SkillTime = JOptionPane.showInputDialog(null, "Enter time to complete skill:");
-                        String Category = JOptionPane.showInputDialog(null, "Enter the skills category:");
-                        if (SkillXP != null) {
-                            try {
-                                int XPNumber = Integer.parseInt(SkillXP);
-                                JOptionPane.showMessageDialog(null, "The skills name is: " + SkillName + "\nThe skills goal is : " + SkillGoal + "\nThe skills XP is: " + XPNumber + "\nThe time to complete the skill is: " + SkillTime + "\nThe skills category is: " + Category);
+        skillTreeButton.addActionListener(e -> {
+            JFrame SkillTreeFrame = new JFrame("Skill Tree");
+            SkillTreeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            SkillTreeFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            Main.dispose();
+            // add the code for the popup menu here
+            JPopupMenu popupMenu = new JPopupMenu();
+            JMenuItem CreateSkill = new JMenuItem("Create Skill");
+            CreateSkill.addActionListener(e1 -> {
+                String SkillName = JOptionPane.showInputDialog(null, "Enter skill name:");
+                String SkillGoal = JOptionPane.showInputDialog(null, "Enter the skills goal:");
+                String SkillXP = JOptionPane.showInputDialog(null, "Enter the skills XP:");
+                String SkillTime = JOptionPane.showInputDialog(null, "Enter time to complete skill:");
+                String Category = JOptionPane.showInputDialog(null, "Enter the skills category:");
+                if (SkillXP != null) {
+                    try {
+                        int XPNumber = Integer.parseInt(SkillXP);
+                        JOptionPane.showMessageDialog(null, "The skills name is: " + SkillName + "\nThe skills goal is : " + SkillGoal + "\nThe skills XP is: " + XPNumber + "\nThe time to complete the skill is: " + SkillTime + "\nThe skills category is: " + Category);
 
 
-                            } catch (NumberFormatException ex) {
-                                JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.");
-                                CreateSkill();
-                            }
-                        }
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.");
+                        CreateSkill();
                     }
-
-                });
-
-
-                JMenuItem CreateChallenge = new JMenuItem("Create Challenge");
-                CreateChallenge.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        String ChallengeName = JOptionPane.showInputDialog(null, "Enter the challenges name:");
-                        String ChallengeGoal = JOptionPane.showInputDialog(null, "Enter the challenges goal:");
-                        String ChallengeXP = JOptionPane.showInputDialog(null, "Enter the challenges XP:");
-                        if (ChallengeXP != null) {
-                            try {
-                                int XPNumber = Integer.parseInt(ChallengeXP);
-                                JOptionPane.showMessageDialog(null, "The challenges name is: " + ChallengeName + "\nThe challenges goal is : " + ChallengeGoal + "\nThe challenges XP is: " + XPNumber);
+                }
+            });
 
 
-                            } catch (NumberFormatException ex) {
-                                JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.");
-                                CreateChallenge();
-                            }
-                        }
+            JMenuItem CreateChallenge = new JMenuItem("Create Challenge");
+            CreateChallenge.addActionListener(e12 -> {
+                String ChallengeName = JOptionPane.showInputDialog(null, "Enter the challenges name:");
+                String ChallengeGoal = JOptionPane.showInputDialog(null, "Enter the challenges goal:");
+                String ChallengeXP = JOptionPane.showInputDialog(null, "Enter the challenges XP:");
+                if (ChallengeXP != null) {
+                    try {
+                        int XPNumber = Integer.parseInt(ChallengeXP);
+                        JOptionPane.showMessageDialog(null, "The challenges name is: " + ChallengeName + "\nThe challenges goal is : " + ChallengeGoal + "\nThe challenges XP is: " + XPNumber);
+
+
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.");
+                        CreateChallenge();
                     }
+                }
+            });
+            JMenuItem CreateItem = new JMenuItem("Create Item");
+            CreateItem.addActionListener(e13 -> {
+                String ItemName = JOptionPane.showInputDialog(null, "Enter the items name:");
+                String ItemEmoji = JOptionPane.showInputDialog(null, "get emojis from https://emojipedia.org\nEnter the items emoji:");
+                String ItemLink = JOptionPane.showInputDialog(null, "Enter the items Link");
+                JOptionPane.showMessageDialog(null, "The items name is: " + ItemName + "\nThe item emoji is: " + ItemEmoji + "\nThe items link is: " + ItemLink);
+            });
+            JMenuItem CreateNewTree = new JMenuItem("Create new skill tree");
+            CreateNewTree.addActionListener(e14 -> {
+                String TreeName = JOptionPane.showInputDialog(null, "Enter skill tree name:");
+                JFrame NewSkillTree = new JFrame(TreeName);
+                NewSkillTree.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                NewSkillTree.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                NewSkillTree.setVisible(true);
+                JPanel SkillTreePanel = new JPanel();
+                SkillTreePanel.setLayout(new FlowLayout());
+                SkillTreeFrame.dispose();
+                JButton MenuButton = new JButton("Menu");
+                MenuButton.addActionListener(e141 -> {
+                    NewSkillTree.setVisible(false);
+                    Main.setVisible(true);
                 });
-                JMenuItem CreateItem = new JMenuItem("Create Item");
-                CreateItem.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        String ItemName = JOptionPane.showInputDialog(null, "Enter the items name:");
-                        String ItemEmoji = JOptionPane.showInputDialog(null, "get emojis from https://emojipedia.org\nEnter the items emoji:");
-                        String ItemLink = JOptionPane.showInputDialog(null, "Enter the items Link");
-                        JOptionPane.showMessageDialog(null, "The items name is: " + ItemName + "\nThe item emoji is: " + ItemEmoji + "\nThe items link is: " + ItemLink);
-                    }
-                });
-                JMenuItem CreateNewTree = new JMenuItem("Create new skill tree");
-                CreateNewTree.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        String TreeName = JOptionPane.showInputDialog(null, "Enter skill tree name:");
-                        JFrame NewSkillTree = new JFrame(TreeName);
-                        NewSkillTree.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        NewSkillTree.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                        NewSkillTree.setVisible(true);
-                        JPanel SkillTreePanel = new JPanel();
-                        SkillTreePanel.setLayout(new FlowLayout());
-                        SkillTreeFrame.dispose();
-                        JButton MenuButton = new JButton("Menu");
-                        MenuButton.addActionListener(new ActionListener() {
-                            public void actionPerformed(ActionEvent e) {
-                                NewSkillTree.setVisible(false);
-                                Main.setVisible(true);
-                            }
-                        });
-                        SkillTreePanel.add(MenuButton);
-                        NewSkillTree.add(SkillTreePanel);
-                        NewSkillTree.addMouseListener(new MouseAdapter() {
-                            public void mousePressed(MouseEvent e) {
-                                if (SwingUtilities.isRightMouseButton(e)) {
-                                    popupMenu.show(e.getComponent(), e.getX(), e.getY());
-                                }
-                            }
-                        });
-                    }
-                });
-                popupMenu.add(CreateSkill);
-                popupMenu.add(CreateChallenge);
-                popupMenu.add(CreateItem);
-                popupMenu.add(CreateNewTree);
-
-                SkillTreeFrame.addMouseListener(new MouseAdapter() {
-                    public void mousePressed(MouseEvent e) {
-                        if (SwingUtilities.isRightMouseButton(e)) {
-                            popupMenu.show(e.getComponent(), e.getX(), e.getY());
+                SkillTreePanel.add(MenuButton);
+                NewSkillTree.add(SkillTreePanel);
+                NewSkillTree.addMouseListener(new MouseAdapter() {
+                    public void mousePressed(MouseEvent e14) {
+                        if (SwingUtilities.isRightMouseButton(e14)) {
+                            popupMenu.show(e14.getComponent(), e14.getX(), e14.getY());
                         }
                     }
                 });
+            });
+            popupMenu.add(CreateSkill);
+            popupMenu.add(CreateChallenge);
+            popupMenu.add(CreateItem);
+            popupMenu.add(CreateNewTree);
 
-                JButton skillTreeExitButton = new JButton("Exit Skill Tree");
-                skillTreeExitButton.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        SkillTreeFrame.dispose();
-                        Main.setVisible(true);
+            SkillTreeFrame.addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    if (SwingUtilities.isRightMouseButton(e)) {
+                        popupMenu.show(e.getComponent(), e.getX(), e.getY());
                     }
-                });
-                JPanel skillTreePanel = new JPanel();
-                skillTreePanel.setLayout(new FlowLayout());
-                skillTreePanel.add(skillTreeExitButton);
-                SkillTreeFrame.add(skillTreePanel);
+                }
+            });
 
-                SkillTreeFrame.pack();
-                SkillTreeFrame.setVisible(true);
-            }
+            JButton skillTreeExitButton = new JButton("Exit Skill Tree");
+            skillTreeExitButton.addActionListener(e15 -> {
+                SkillTreeFrame.dispose();
+                Main.setVisible(true);
+            });
+            JPanel skillTreePanel = new JPanel();
+            skillTreePanel.setLayout(new FlowLayout());
+            skillTreePanel.add(skillTreeExitButton);
+            SkillTreeFrame.add(skillTreePanel);
+
+            SkillTreeFrame.pack();
+            SkillTreeFrame.setVisible(true);
         });
 
         MainPanel.add(skillTreeButton);
